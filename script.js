af67412bd02282a8cf1eb82d2aa4e573d68283f4
Virgil Manalo <virgilio.manalo98@gmail.com>
2020-02-07 19:35:32 -0800

    counter commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// JavaScript DOC
+var pics = [
+"imgs/kitty_bed.jpg", //0
+"imgs/kitty_basket.jpg", //1
+"imgs/kitty_laptop.jpg", //2
+"imgs/kitty_door.jpg", //3
+"imgs/kitty_sink.jpg", //4
+"imgs/kitty_wall.jpg" //5
+];
+
+var btn = document.querySelector("button");
+var img = document.querySelector("img");
+var counter = 1;
+
+btn.addEventListener("click", function(){
+    if(counter === 6){
+        counter = 0;
+    }
+    img.src = pics[counter]
+    counter = counter + 1;
+});
+
